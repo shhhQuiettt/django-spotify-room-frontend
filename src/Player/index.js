2df4b8cd8db39e38f95faf2209509e2042c6bfa6
shhhQuiettt <krzysztof.skro@gmail.com>
2022-06-28 13:50:24 +0200

    create player styling

--- src/Player/index.js
+++ src/Player/index.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react";
+import "./index.css";
 import { FaPlay, FaPause } from "react-icons/fa";
 import { BsFillSkipEndFill, BsFillSkipStartFill } from "react-icons/bs";
 
 const Player = () => {
   let testData = {
     title: "Piosenka",
-    duration_s: 3.43,
-    progress_s: 1.1,
+    duration_s: 194,
+    progress_s: 23,
     album_cover_url:
       "https://freepsdflyer.com/wp-content/uploads/2021/06/Free-Spotify-Album-Cover-PSD-Template.jpg",
     is_playing: false,
     song_id: "123445",
   };
   const [currentTrack, setCurrentTrack] = useState(testData);
+  const [currentSecond, setCurrentSecond] = useState(50);
 
   return (
     <div className="player">
       <img src={currentTrack["album_cover_url"]} alt="" />
-      <div className="song-title">{currentTrack["title"]}</div>
-      <div className="control-panel">
-        <button className="previous">
-          <BsFillSkipStartFill />
-        </button>
-        <button className="play-pause">
-          {currentTrack["is_playing"] ? <FaPlay /> : <FaPause />}
-        </button>
-        <button className="skip">
-          <BsFillSkipEndFill />
-        </button>
+      <div className="wrapper">
+        <div className="song-title">{currentTrack["title"]}</div>
+        <div className="progress-panel">
+          <div className="current-s">
+            {Math.floor(currentSecond / 60)}:{currentSecond % 60}
+          </div>
+          <div className="status-bar">
+            <div
+              className="status-bar-progress"
+              style={{
+                width: (100 * currentSecond) / currentTrack["duration_s"] + "%",
+              }}
+            ></div>
+          </div>
+          <div className="song-length">{currentTrack["duration_s"]}</div>
+        </div>
+        <div className="control-panel">
+          <button className="previous">
+            <BsFillSkipStartFill />
+          </button>
+          <button className="play-pause">
+            {currentTrack["is_playing"] ? <FaPlay /> : <FaPause />}
+          </button>
+          <button className="skip">
+            <BsFillSkipEndFill />
+          </button>
+        </div>
       </div>
-      <div className="status-bar"></div>
     </div>
   );
 };
