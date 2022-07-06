1e446a83017c213b172c43a281c4d8d5df14d328
shhhQuiettt <krzysztof.skro@gmail.com>
2022-07-06 12:37:27 +0200

    reValidation trigger changed

--- src/components/JoinRoomForm/index.js
+++ src/components/JoinRoomForm/index.js
@@ -4,7 +4,11 @@ import "./index.css";
 import { joinRoom } from "../../service";
 
 const JoinRoomForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ reValidateMode: "onSubmit" });
 
   const onSubmit = (data) => {
     console.log("f");
@@ -21,7 +25,14 @@ const JoinRoomForm = () => {
         id="room-code"
         {...register("code", { maxLength: 6, minLength: 6 })}
         maxLength={6}
+        autoComplete="off"
       />
+      {errors.code?.type === "minLength" && (
+        <div className="error-field">Code is too short</div>
+      )}
+      {errors.code?.type === "maxLength" && (
+        <div className="error-field">Code is too long</div>
+      )}
       <button type="submit">Join!</button>
     </form>
   );
