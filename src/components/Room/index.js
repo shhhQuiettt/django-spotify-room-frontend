a4cd68723da2e9ad05a69b43494ed22a01bd1f04
shhhQuiettt <krzysztof.skro@gmail.com>
2022-07-11 13:23:39 +0200

    NavButtons created

--- src/components/Room/index.js
+++ src/components/Room/index.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Room = ({ code }) => {
   let navigate = useNavigate();
   useEffect(() => {
-    if (!localStorage.getItem("roomCode")) {
+    if (!code) {
       navigate("../room/join", { replace: true });
     }
   }, []);
